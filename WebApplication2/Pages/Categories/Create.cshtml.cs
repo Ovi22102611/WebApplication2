8b7dd1dd2b2e65df70a7cd39a3d381ea8bee5540
Ovi22102611 <junaedrahman8@gmail.com>
2023-11-23 07:43:53 +0000

    binding input tag in a category model

--- WebApplication2/Pages/Categories/Create.cshtml.cs
+++ WebApplication2/Pages/Categories/Create.cshtml.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication2.Model;
 
 namespace WebApplication2.Pages.Categories
 {
     public class CreateModel : PageModel
     {
+public Category Category { get; set; }
         public void OnGet()
         {
         }
