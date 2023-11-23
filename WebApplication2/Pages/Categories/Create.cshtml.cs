c20ad95309bde4d954c77c7d61ef450925cdf3ab
Ovi22102611 <junaedrahman8@gmail.com>
2023-11-23 05:55:21 +0000

    Add project files.

---
+++ WebApplication2/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication2.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
